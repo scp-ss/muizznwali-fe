648606f1d748980533728e180f8013d071793ca0
Muizz Nasir <muizznasir04@gmail.com>
2025-08-08 11:26:22 +0500

    UPdated Whatsapp Position

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,24 @@ import PrivacyPolicy from "./pages/heavy-duty-calculator/privacy-policy/privacy-
 import "./App.css";
 import WhatsAppSection from "./pages/whatsapp/whatsapp";
 export default function App() {
-  const location = useLocation();
-  const muizz = "If you are seeing ts it means u are either a dev that found foss project or u are a HAKOR OMG OMG PLZ PZL Z LZPZLP, "
+const location = useLocation();
+const muizz = "If you are seeing ts it means u are either a dev that found foss project or u are a HAKOR OMG OMG PLZ PZL Z LZPZLP, "
   
   return (
     <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
       {/*  Navbar */}
-       <p id="twk"> {muizz}</p>
       <nav className="navbar">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/heavy-duty-calculator" className="nav-link">Heavy Duty Calculator</Link>
       </nav>
-      <WhatsAppSection/>
+
 
       {/*  Routes */}
       <Routes>
         <Route path="/heavy-duty-calculator" element={<HeavyDutyCalculator />} />
         <Route path="/heavy-duty-calculator/privacypolicy" element={<PrivacyPolicy />} />
       </Routes>
-
+<WhatsAppSection/>
       {/* ⬅️ Back Button (only show if not on home) */}
       {location.pathname !== "/" && (
         <div className="back-to-home">
