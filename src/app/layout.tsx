ed1f23749982e33d7eba3ccba8072de621ea5506
Muizz Nasir <muizznasir04@gmail.com>
2025-08-11 22:35:51 +0500

    ADDED HILTOPADS 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar/navbar";
 import "@/css/astetics.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"; // ✅ Correct
 
 export const metadata: Metadata = {
   title: "Muizz N Wali",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        
         <meta name="referrer" content="no-referrer-when-downgrade" />
         <meta name="fc1279abd204ac51eb717a7fed308fbe12f00504" content="fc1279abd204ac51eb717a7fed308fbe12f00504" />
         {/*{process.env.NODE_ENV === "production" && (
@@ -21,7 +23,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
      crossOrigin="anonymous"></script>)
         }*/}
         {/*<meta name="google-adsense-account" content="ca-pub-8243831335530194">*/}
+        <Script id="ad-loader" strategy="afterInteractive">
+          {`(function(qvks){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = qvks || {};
+s.src = "\/\/flashy-explanation.com\/ckD.9j6rb\/2\/5qlcSAWtQm9uNhj-UC4\/MTzRQ\/zsNyiM0T2\/NCT\/g\/z\/N\/D\/M\/3R";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})`}
+</Script>
       </head>
+      
       <body className="bg-gray-50 text-gray-900 antialiased">
         <Navbar />
         <SpeedInsights/>
