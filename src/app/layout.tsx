70a01c02091e5bd01aa2580fcdf6daa0fcc04c20
Muizz Nasir <muizznasir04@gmail.com>
2025-08-11 21:49:52 +0500

    ADDED ANALYTICS AND DATA COLLECTION

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import "./../appstyles/css/globals.css";
 import Navbar from "@/components/navbar/navbar";
 import "@/css/astetics.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Muizz N Wali",
@@ -21,6 +22,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className="bg-gray-50 text-gray-900 antialiased">
         <Navbar />
+        <SpeedInsights/>
+        <Analytics />
         <main className="max-w-7xl mx-auto px-4 py-8">{children}</main>
       </body>
     </html>
