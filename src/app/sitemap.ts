b6ce79aa84dd39845759f6563838a1e9eb1ce3ce
Muizz Nasir <muizznasir04@gmail.com>
2025-09-13 16:09:01 +0500

    ADDED SITEMAP.XML AND ROBOTS.TXT

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,72 +1,70 @@
-import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap() {
   const baseUrl = 'https://muizznwali.com'
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/admin`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/admin/login`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/ai`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/ai/chatbot`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/ai/drawing`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/downloads`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/Extraapps`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/Extraapps/SpaceAdderAndCapitalizer`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/heavy-duty-calculator`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/heavy-duty-calculator/app/delete`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/heavy-duty-calculator/privacy-policy`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/test/todo/drawing/svg`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/test-firebase`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/todo`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${baseUrl}/todo/test/imagebuttons`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
   ]
 }
